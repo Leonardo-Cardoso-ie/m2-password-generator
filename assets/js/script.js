2db5b6bf4d54574d50321164e059c2f6dfca6bb3
Leonardo-Cardoso-ie <leonardocardoso.ie@gmail.com>
2023-01-02 08:23:08 +0000

    Add variables and characters to app

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/*Giving functionality to the app*/
+
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#value");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+/**
+ * Characters
+ */
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@-_$^&?><";
